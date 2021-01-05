b9702eade0cb0c8741241cdba32c544d66f44d33
Alessandro Ramos <toprime13@gmail.com>
2021-01-05 17:52:57 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, } from 'react-router-dom';
 
 import Header from './components/Header/index';
 import Routes from './pages/routes';
 //import Footer from './components/Footer/index';
+
+// import { Container } from './styles';
 import GlobalStyles from './styles/GlobalStyles';
 
-const App = () => {
-  return (
+const App: React.FC = () => {
+  return(
     <Router>
+
       <Header />
 
       <Routes />
 
-      {/*<Footer />*/}
+      {/*<Footer /> */}
 
       <GlobalStyles />
+
     </Router>
   );
 }
