adbc7f4cefd3cea4d7c59fec1df295930bed2e27
Alessandro Ramos <toprime13@gmail.com>
2021-01-06 22:01:39 -0300

    adicionado rota 404

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Overview from './Overview/index';
 import Repositories from './Repositories/index';
+import Error404 from '../components/Error404/index';
 
 // import { Container } from './styles';
 
@@ -12,7 +13,7 @@ const Pages: React.FC = () => {
       <Route path='/' element={<Overview />} />
       <Route path='/:username' element={<Overview />} />
       <Route path='/:username/?tab=repositories' element={<Repositories />} />
-      {/*Route path='*' element={<>} />*/}
+      <Route path='*' element={<Error404 />} />
     </Routes>
   );
 }
