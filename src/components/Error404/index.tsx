0443baa13700800939c2dae6f91d535ed5f473b8
Alessandro Ramos <toprime13@gmail.com>
2021-01-06 21:54:45 -0300

    adiionado Error404

---
+++ src/components/Error404/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Error404: React.FC = () => {
+  return(
+    <Container>
+      <h1>Error 404</h1>
+      <div>
+        <p>This is not the webpage you are looking for</p>
+      </div>
+    </Container>
+  );
+}
+
+export default Error404;
---
+++ src/components/Error404/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+  flex-direction: column;
+
+  > h1 {
+    font-size: 5rem;
+  }
+
+  > div {
+    > p {
+      font-size: 2rem;
+    }
+  }
+`;
+
