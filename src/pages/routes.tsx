d9004e6103a859be990648d940b435f638841c99
Alessandro Ramos <toprime13@gmail.com>
2021-01-07 21:55:14 -0300

    update routes

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import Overview from './Overview/index';
-import Repositories from './Repositories/index';
-import Error404 from '../components/Error404/index';
-
-// import { Container } from './styles';
+import Profile from "./Profile/index";
+import Overview from "./Profile/Overview/index";
+import Repositories from "./Profile/Repositories/index";
+import Error404 from "../components/Error404/index";
 
 const Pages: React.FC = () => {
-  return(
+  return (
     <Routes>
-      <Route path='/' element={<Overview />} />
-      <Route path='/:username' element={<Overview />} />
-      <Route path='/:username/?tab=repositories' element={<Repositories />} />
-      <Route path='*' element={<Error404 />} />
+      <Route path="/" element={<Navigate to="/alessandrohenriqueramos" />} />
+      <Route path="/:username" element={<Profile />}>
+        <Route path="/" element={<Overview />} />
+        <Route path="/repositories" element={<Repositories />} />
+      </Route>
+      <Route path="*" element={<Error404 />} />
     </Routes>
   );
-}
+};
 
 export default Pages;
